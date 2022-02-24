8c221d08daea87b6294299d112384a1a2f5b702b
cicinth <cinthiaqcardoso@hotmail.com>
2022-02-23 21:03:25 -0300

    feat: add endpoint for get one user and update

--- src/main/java/com/aula04/banco/banco/controller/ClienteController.java
+++ src/main/java/com/aula04/banco/banco/controller/ClienteController.java
@@ -6,10 +6,13 @@ import com.aula04.banco.banco.model.BancoCliente;
 import com.aula04.banco.banco.model.Cliente;
 import com.aula04.banco.banco.model.Conta;
 import com.aula04.banco.banco.model.TipoConta;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.util.UriComponentsBuilder;
 
+import java.net.URI;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Random;
@@ -29,7 +32,10 @@ public class ClienteController {
     }
 
     @PostMapping
-    public ResponseCliente cadastraCliente(@RequestBody RequestCliente requestCliente){
+    public ResponseEntity<ResponseCliente> cadastraCliente(
+            @RequestBody RequestCliente requestCliente,
+            UriComponentsBuilder uriComponentsBuilder
+    ){
         List<Conta> contas = new ArrayList<>();
         Conta conta = new Conta(UUID.randomUUID(), random.nextInt(), requestCliente.getAgencia(), TipoConta.CONTA_CORRENTE);
         contas.add(conta);
@@ -41,6 +47,22 @@ public class ClienteController {
                 contas
         );
         bancoCliente.adiciona(cliente);
-        return new ResponseCliente(cliente);
+
+        URI uri = uriComponentsBuilder.path("/cliente/{id}").buildAndExpand(cliente.getId()).toUri();
+        return ResponseEntity.created(uri).body(new ResponseCliente(cliente));
+    }
+    @GetMapping("/{id}")
+    public ResponseEntity<ResponseCliente> detalhesCliente(@PathVariable UUID id) throws Exception {
+        Cliente cliente = bancoCliente.detalhesCliente(id);
+        return ResponseEntity.ok(new ResponseCliente(cliente));
+    }
+    @PutMapping("{id}")
+    public ResponseEntity<ResponseCliente> atualizaCliente(
+            @PathVariable UUID id,
+            @RequestBody RequestCliente requestCliente
+    ) throws Exception {
+        Cliente cliente = bancoCliente.atualizaCliente(id, requestCliente);
+
+        return ResponseEntity.ok(new ResponseCliente(cliente));
     }
 }
--- src/main/java/com/aula04/banco/banco/model/BancoCliente.java
+++ src/main/java/com/aula04/banco/banco/model/BancoCliente.java
@@ -1,7 +1,8 @@
 package com.aula04.banco.banco.model;
 
-import java.util.ArrayList;
-import java.util.List;
+import com.aula04.banco.banco.dto.RequestCliente;
+
+import java.util.*;
 
 public class BancoCliente {
     private static List<Cliente> clientes = new ArrayList<>();
@@ -12,5 +13,25 @@ public class BancoCliente {
 
     public List<Cliente> buscaClientes(){
         return BancoCliente.clientes;
+    }
+
+    public Cliente detalhesCliente(UUID id) throws Exception {
+           Optional<Cliente> resultCliente =
+                   BancoCliente.clientes.stream().filter(cliente -> Objects.equals(cliente.getId(),id)).findAny();
+           if(resultCliente.isPresent()){
+               return resultCliente.get();
+           } else {
+               throw new Exception("Usuario nao encontrado");
+           }
+    }
+
+    public Cliente atualizaCliente(UUID id, RequestCliente requestCliente) throws Exception {
+        BancoCliente.clientes.stream().filter(cliente -> Objects.equals(cliente.getId(),id))
+                .forEach(cliente -> {
+                    cliente.setNome(requestCliente.getNome());
+                    cliente.setEmail(requestCliente.getEmail());
+                    cliente.setSenha(requestCliente.getSenha());
+                });
+        return detalhesCliente(id);
     }
 }
